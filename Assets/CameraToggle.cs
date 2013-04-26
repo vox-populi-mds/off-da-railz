bf92450fdf36f911101379fc0b669e0aa5184286
Kit is la! <kit@mds.com>
2013-04-26 13:00:59 +1200

    Camera Improvements
    Camera Prefab now automagically binds to CenterOfMass if nothing is
    selected

--- Assets/CameraToggle.cs
+++ Assets/CameraToggle.cs
@@ -4,11 +4,34 @@ using System.Collections;
 public class CameraToggle : MonoBehaviour {
 	public Camera[] Cameras;
 	private int cameraIndex = 0;
+	public Transform m_target;
 	
 	// Use this for initialization
 	void Start () {
 		cameraIndex = 0;
 		
+		foreach (Camera Cam in Camera.allCameras) 
+		{
+			Cam.gameObject.SetActive (false);
+		}
+		
+		Cameras[cameraIndex].gameObject.SetActive(true);
+	}
+	
+	void Awake () {
+		
+		if (!m_target)
+			m_target = GameObject.Find("CenterOfMass").transform;
+		
+		foreach (PositionSmoothFollow psf in GetComponentsInChildren<PositionSmoothFollow>(true))
+		{
+			psf.m_target = m_target;
+		}
+		
+		foreach (SmoothFollow sf in GetComponentsInChildren<SmoothFollow>(true))
+		{
+			sf.target = m_target;
+		}
 	}
 	
 	// Update is called once per frame
