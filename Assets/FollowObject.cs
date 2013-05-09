773d4a88b3fa9283e755d2137d66117a87ba1d79
Daniel Langsford <folduppugg@hotmail.com>
2013-05-10 10:52:06 +1200

    Kinda better I guess, maybe

--- Assets/FollowObject.cs
+++ Assets/FollowObject.cs
@@ -7,9 +7,7 @@ public class FollowObject : MonoBehaviour {
 public Transform target;
 public float distance = 1.0f;
 public float rotationDamping = 4.0f;
-public float heightDamping = 2.0f;
-public float PosDamping = 10;	
-public int NumWaypoints = 5;	
+public int NumWaypoints = 10;	
 public bool DebugMode = false;
 protected GameObject[] DummyWaypoints;
 
@@ -22,8 +20,7 @@ protected GameObject[] DummyWaypoints;
 			for(int i = 0; i < NumWaypoints; i++)
 			{
 				DummyWaypoints[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-				Destroy(DummyWaypoints[i].GetComponent("collider"));
-				Destroy(DummyWaypoints[i].GetComponent("renderer"));
+				Destroy(DummyWaypoints[i].GetComponent<SphereCollider>());		
 			}	
 		}
 		else
@@ -33,8 +30,8 @@ protected GameObject[] DummyWaypoints;
 			for(int i = 0; i < NumWaypoints; i++)
 			{
 				DummyWaypoints[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-				Destroy(DummyWaypoints[i].GetComponent("collider"));
-				Destroy(DummyWaypoints[i].GetComponent("renderer"));				
+				Destroy(DummyWaypoints[i].GetComponent<SphereCollider>());
+				Destroy(DummyWaypoints[i].GetComponent<Renderer>());				
 			}	
 		}
 	}
@@ -45,30 +42,23 @@ protected GameObject[] DummyWaypoints;
 		{
 			return;
 		}
-		
-		//Set first dummy object to follow target	
-		float wantedRotationAngle = target.eulerAngles.y;
-		float currentRotationAngle = DummyWaypoints[0].transform.eulerAngles.y;
 			
-		currentRotationAngle = Mathf.LerpAngle (currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
-		Quaternion currentRotation = Quaternion.Euler (0, currentRotationAngle, 0);
-
+		//Set first dummy object to follow target	
+		Quaternion currentRotation = LerpToRotation(DummyWaypoints[0].transform.eulerAngles, target.eulerAngles);
 		
 		DummyWaypoints[0].transform.position = target.position;
-		DummyWaypoints[0].transform.position -= currentRotation * Vector3.forward * distance / NumWaypoints;			
+		DummyWaypoints[0].transform.position -= currentRotation * Vector3.forward * distance;	
+		
+		//Look at target
 		DummyWaypoints[0].transform.LookAt (target);
 		
 		//Make other dummy objects follow the first		
 		for(int i = 1; i < NumWaypoints; i++)
 		{
-			wantedRotationAngle = DummyWaypoints[i - 1].transform.eulerAngles.y;
-			currentRotationAngle = DummyWaypoints[i].transform.eulerAngles.y;		
-			
-			currentRotationAngle = Mathf.LerpAngle (currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
-			currentRotation = Quaternion.Euler (0, currentRotationAngle, 0);
+			currentRotation = LerpToRotation(DummyWaypoints[i].transform.eulerAngles, DummyWaypoints[i - 1].transform.eulerAngles);
 			
 			DummyWaypoints[i].transform.position = DummyWaypoints[i - 1].transform.position;
-			DummyWaypoints[i].transform.position -= currentRotation * Vector3.forward * distance / NumWaypoints;
+			DummyWaypoints[i].transform.position -= currentRotation * Vector3.forward * distance;
 			
 			DummyWaypoints[i].transform.LookAt (DummyWaypoints[i - 1].transform.position);
 		}
@@ -76,24 +66,10 @@ protected GameObject[] DummyWaypoints;
 		//Now fo' realz, make the object follow the second dummy object. This makes the object appear to follow it's targets path,
 		//rather than the target itself, which looks weird.
 		
-		wantedRotationAngle = DummyWaypoints[NumWaypoints - 1].transform.eulerAngles.y;
-		currentRotationAngle = transform.eulerAngles.y;		
-		
-		currentRotationAngle = Mathf.LerpAngle (currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
-		currentRotation = Quaternion.Euler (0, currentRotationAngle, 0);
+		currentRotation = LerpToRotation(transform.eulerAngles, DummyWaypoints[NumWaypoints - 1].transform.eulerAngles);	
 	
-		//Save height for later before setting it.
-		float currentHeight = transform.position.y;			
 		transform.position = DummyWaypoints[NumWaypoints - 1].transform.position;
-		transform.position -= currentRotation * Vector3.forward * distance / NumWaypoints;
-		
-		//Lerp to target height
-		float wantedHeight = target.position.y;
-		currentHeight = Mathf.Lerp (currentHeight, wantedHeight, heightDamping * Time.deltaTime);
-		
-		Vector3 NewPosition = transform.position;
-		NewPosition.y = currentHeight;
-		transform.position = NewPosition; 
+		transform.position -= currentRotation * Vector3.forward * 15;
 		
 		transform.LookAt (DummyWaypoints[NumWaypoints - 1].transform.position);		
 	}
@@ -101,6 +77,23 @@ protected GameObject[] DummyWaypoints;
 	void SetTarget(Transform _Target)
 	{
 		target = _Target;
+	}
+	
+	Quaternion LerpToRotation (Vector3 CurrentRotation, Vector3 TargetRotation)
+	{
+		float wantedRotationAngleX = TargetRotation.x;
+		float wantedRotationAngleY = TargetRotation.y;
+		float wantedRotationAngleZ = TargetRotation.z;
+		float currentRotationAngleX = CurrentRotation.x;
+		float currentRotationAngleY = CurrentRotation.y;
+		float currentRotationAngleZ = CurrentRotation.z;
+		
+		currentRotationAngleX = Mathf.LerpAngle (currentRotationAngleX, wantedRotationAngleX, rotationDamping * Time.deltaTime);
+		currentRotationAngleY = Mathf.LerpAngle (currentRotationAngleY, wantedRotationAngleY, rotationDamping * Time.deltaTime);
+		currentRotationAngleZ = Mathf.LerpAngle (currentRotationAngleZ, wantedRotationAngleZ, rotationDamping * Time.deltaTime);
+		
+		Quaternion currentRotation = Quaternion.Euler (currentRotationAngleX, currentRotationAngleY, currentRotationAngleZ);
+		return(currentRotation);
 	}
 }
 
