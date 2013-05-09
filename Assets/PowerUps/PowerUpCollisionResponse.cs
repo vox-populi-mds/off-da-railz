a26594ffc67ad4d5a121f6bee40f07384f55eb61
Dustin Dos Santos <Merlin989@gmail.com>
2013-05-09 12:13:19 +1200

    latest commit

--- Assets/PowerUps/PowerUpCollisionResponse.cs
+++ Assets/PowerUps/PowerUpCollisionResponse.cs
@@ -8,14 +8,14 @@ public class PowerUpCollisionResponse : MonoBehaviour {
 	}
 	
 	void OnCollisionEnter(Collision CollsionInfo){
-		TrainCarrages PlayerTrainCarrages;
+		/*TrainCarrages PlayerTrainCarrages;
 		
 		if (CollsionInfo.gameObject.tag == "Train"){
 			PlayerTrainCarrages = CollsionInfo.gameObject.GetComponent<Carrages>();
 			PlayerTrainCarrages.AppendCarrage();
 		}
 		
-		this.renderer.enabled = false;
+		this.renderer.enabled = false;*/
 	}	
 	
 	// Update is called once per frame
