e77dbe372148a449e3debce324bce9aee073afa2
Dustin Dos Santos <Merlin989@gmail.com>
2013-05-10 10:04:55 +1200

    Revert "Latest Changes to shotgun"
    This reverts commit c623349ba3c640947be77f3aafe784f537484d59.

---
+++ Assets/Weapons/Shotgun/Notes.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class Notes : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	//http://soundbible.com/1706-Shot-Gun.html
+}
