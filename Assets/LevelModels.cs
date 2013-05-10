73c678e0e18edb6257ce9da4bf352917e294a2de
Scott <scott.ipod@gmail.com>
2013-05-10 12:38:16 +1200

    Changed design of spawn area
    Changed to prefab setup

--- Assets/LevelModels.cs
+++ Assets/LevelModels.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class LevelModels : MonoBehaviour
 {
 	public Transform cube;
-	public Transform tunnel;
 	
 	GameObject[] Cubes = new GameObject[10];
 	
@@ -28,8 +27,7 @@ public class LevelModels : MonoBehaviour
 			//cubeTransform2.localScale = Scale;
 			
 		}
-		// Spawn Areas
-		PlayerSpawnArea();
+
 
 	}
 	
@@ -39,34 +37,5 @@ public class LevelModels : MonoBehaviour
 	
 	}
 	
-	void PlayerSpawnArea()
-	{
-		// SPAWN TUNNEL A
-		Transform SpawnPointA = (Transform) Instantiate(tunnel, new Vector3(695,94,582), Quaternion.identity);
-		SpawnPointA.localScale = new Vector3(10, 10, 20);
-		Vector3 RotateYA = new Vector3(0, 46, 0);
-		SpawnPointA.Rotate(RotateYA);
-		SpawnPointA.name = "SpawnA";
-		
-		// SPAWN TUNNEL B
-		Transform SpawnPointB = (Transform) Instantiate(tunnel, new Vector3(-664,72,658), Quaternion.identity);
-		SpawnPointB.localScale = new Vector3(10, 10, 20);
-		Vector3 RotateYB = new Vector3(0, 306, 0);
-		SpawnPointB.Rotate(RotateYB);
-		SpawnPointB.name = "SpawnB";
-		
-		// SPAWN TUNNEL C
-		Transform SpawnPointC = (Transform) Instantiate(tunnel, new Vector3(-627,76,-672), Quaternion.identity);
-		SpawnPointC.localScale = new Vector3(10, 10, 20);
-		Vector3 RotateYC = new Vector3(0, -150, 0);
-		SpawnPointC.Rotate(RotateYC);
-		SpawnPointC.name = "SpawnC";		
-		
-		// SPAWN TUNNEL D
-		Transform SpawnPointD = (Transform) Instantiate(tunnel, new Vector3(661, 67 ,-590), Quaternion.identity);
-		SpawnPointD.localScale = new Vector3(10, 10, 20);
-		Vector3 RotateYD = new Vector3(0, 144, 0);
-		SpawnPointD.Rotate(RotateYD);
-		SpawnPointD.name = "SpawnD";
-	}
+
 }
